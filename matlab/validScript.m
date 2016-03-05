80fe978fe7cadb1e568ea06b81327c57179d80bb
Phillip Chilson <chilson@ou.edu>
2016-03-05 07:51:54 -0600

    revise calibScript and add validScript

--- matlab/validScript.m
+++ matlab/validScript.m
@@ -1,7 +1,7 @@
 function calibScript(procYear, procMonth, procDay, sensorType, offsetFlag, quickLookFlag)
-% calibScript: Used to calibrate sensors
+% validScript: Used to validate sensors
 % Function call:
-% calibScript(procYear, procMonth, procDay, sensorType, offsetFlag, quickLookFlag)
+% validScript(procYear, procMonth, procDay, sensorType, offsetFlag, quickLookFlag)
 
 clc
 
@@ -15,8 +15,8 @@ switch nargin
             'Month:', ...
             'Day:', ...
             'Sensor (Windsond or iMet):', ...
-            'quickLookFlag (0 or 1 or blank):', ...
-            'offsetFlag (0 or 1 of blank):'};
+            'offsetFlag (0 or 1 or blank):', ...
+            'quickLookFlag (0 or 1 or blank):'};
         %
         LineNo = 1;
         %
@@ -58,7 +58,7 @@ switch sensorType
         return
 end
 
-procStation = 'nwcm';
+procStation = 'wash';
 % set fetchFlag to 1 if mesonet data retrieved automatically (mac)
 % set fetchFlag to 0 if you will download the file by hand (PC)
 fetchFlag = 1;
@@ -172,83 +172,19 @@ for iVal = 1: nVals
     end
 end
 
-% Produce quick plot for sanity check and to determine if bounds should be
-% set before calculating offsets
-figure(1)
-clf
-% ------------------------------------------
-subplot(3, 1, 1)
-% ------------------------------------------
-plot(sensor.obsTime(indSensor), sensor.pressure_Pa(indSensor)/100, 'r')
-hold on
-plot(sensorObsTimeAvg, sensorPressureAvg_Pa/100, '*-b', 'linewidth', 2)
-plot(mts.obsTime(indMeso), mts.pressure_Pa(indMeso)/100, '*-k', 'linewidth', 2)
-hold off
-set(gca, 'xlim', [timeBeg timeEnd])
-datetick('x', 13, 'keeplimits')
-xlabel('Time UTC')
-% ------------------------------------------
-subplot(3, 1, 2)
-% ------------------------------------------
-plot(sensor.obsTime(indSensor), sensor.temperature_C(indSensor), 'r')
-hold on
-plot(sensorObsTimeAvg, sensorTemperatureAvg_C, '*-b', 'linewidth', 2)
-plot(mts.obsTime(indMeso), mts.temperature1p5m_C(indMeso), '*-k', 'linewidth', 2)
-hold off
-set(gca, 'xlim', [timeBeg timeEnd])
-datetick('x', 13, 'keeplimits')
-xlabel('Time UTC')
-ylabel('Temperature (C)')
-% ------------------------------------------
-subplot(3, 1, 3)
-% ------------------------------------------
-plot(sensor.obsTime(indSensor), sensor.humidity_perCent(indSensor), 'r')
-hold on
-plot(sensorObsTimeAvg, sensorHumidityAvg_perCent, '*-b', 'linewidth', 2)
-plot(mts.obsTime(indMeso), mts.humidity_perCent(indMeso), '*-k', 'linewidth', 2)
-hold off
-set(gca, 'xlim', [timeBeg timeEnd])
-datetick('x', 13, 'keeplimits')
-xlabel('Time UTC')
-ylabel('Relative Humidity (%)')
-% ------------------------------------------
-reply = input('Select a range of values for the offset calculation? (n/y): ', 's');
-if isempty(reply), reply = 'n'; end
-if upper(reply) == 'Y'
-    fprintf('Click on plot to select times\n')
-    [timeLim, ~] = ginput(2);
-else
-    timeLim = [timeBeg; timeEnd];
-end
-ind1 = find(timeLim(1) <= sensorObsTimeAvg & sensorObsTimeAvg <= timeLim(2));
-ind2 = find(timeLim(1) <= mts.obsTime & mts.obsTime <= timeLim(2));
 % ------------------------------------------
 % Calculate the offsets
-offsetPressure_Pa = -nanmean(sensorPressureAvg_Pa(ind1) - mts.pressure_Pa(ind2));
-offsetTemperature_C = -nanmean(sensorTemperatureAvg_C(ind1) - mts.temperature1p5m_C(ind2));
-offsetHumidity_perCent = -nanmean(sensorHumidityAvg_perCent(ind1) - mts.humidity_perCent(ind2));
-fprintf('\n*** Calculated Offset Values ***\n')
-fprintf('* Time interval for offset calculation\n');
-fprintf('Start Time: %s\n', datestr(timeLim(1)));
-fprintf('Stop Time : %s\n', datestr(timeLim(2)));
-fprintf('* Amount to be added to sensor\n');
-fprintf('Pressure (Pa): %f\n', offsetPressure_Pa);
-fprintf('Temperature (C): %f\n', offsetTemperature_C);
-fprintf('Humidity (percent): %f\n', offsetHumidity_perCent);
-% If offset flag set, print the offset file
+
 if offsetFlag
-    ind = strfind(sensorFileName, '.');
-    offsetFileName = strrep(sensorFileName, sensorFileName(ind: end), '_offset.txt');
-    fp = fopen([ dataDirName offsetFileName ], 'wt');
-    fprintf(fp, '*** Calculated Offset Values ***\n');
-    fprintf(fp, '* Time interval for offset calculation\n');
-    fprintf(fp, 'Start Time: %s\n', datestr(timeLim(1)));
-    fprintf(fp, 'Stop Time : %s\n', datestr(timeLim(2)));
-    fprintf(fp, '* Amount to be added to sensor\n');
-    fprintf(fp, 'Pressure (Pa): %f\n', offsetPressure_Pa);
-    fprintf(fp, 'Temperature (C): %f\n', offsetTemperature_C);
-    fprintf(fp, 'Humidity (percent): %f\n', offsetHumidity_perCent);
-    fclose(fp);
+    % need values here for particular sensor ... set to 0 for now
+    offsetPressure_Pa = 0;
+    offsetTemperature_C = 0;
+    offsetHumidity_perCent = 0;
+else
+    % set offsets to 0
+    offsetPressure_Pa = 0;
+    offsetTemperature_C = 0;
+    offsetHumidity_perCent = 0;
 end
 
 %% Plot the data
@@ -259,17 +195,13 @@ axisWidth = 1.5;
 figure(1)
 % --------------------------------------
 clf
-plot(sensor.obsTime(indSensor), sensor.pressure_Pa(indSensor)/100, 'r', ...
+plot(sensor.obsTime(indSensor), (sensor.pressure_Pa(indSensor) + offsetPressure_Pa)/100, 'r', ...
     'linewidth', lineWidth)
 hold on
-plot(sensorObsTimeAvg, sensorPressureAvg_Pa/100, '*-b', 'linewidth', 2, ...
+plot(sensorObsTimeAvg, (sensorPressureAvg_Pa + offsetPressure_Pa)/100, '*-b', 'linewidth', 2, ...
     'linewidth', lineWidth)
 plot(mts.obsTime(indMeso), mts.pressure_Pa(indMeso)/100, '*-k', 'linewidth', 2, ...
     'linewidth', lineWidth)
-plot(sensorObsTimeAvg, (sensorPressureAvg_Pa + offsetPressure_Pa)/100, '*-g', 'linewidth', 2, ...
-    'linewidth', lineWidth)
-plot(timeLim(1)*[1 1], get(gca, 'ylim'), '-k', 'linewidth', lineWidth)
-plot(timeLim(2)*[1 1], get(gca, 'ylim'), '-k', 'linewidth', lineWidth)
 hold off
 set(gca, 'fontsize', fontSize)
 set(gca, 'linewidth', axisWidth)
@@ -290,17 +222,13 @@ end
 figure(2)
 % --------------------------------------
 clf
-plot(sensor.obsTime(indSensor), sensor.temperature_C(indSensor), 'r', ...
+plot(sensor.obsTime(indSensor), sensor.temperature_C(indSensor) + offsetTemperature_C, 'r', ...
     'linewidth', lineWidth)
 hold on
-plot(sensorObsTimeAvg, sensorTemperatureAvg_C, '*-b', 'linewidth', 2, ...
+plot(sensorObsTimeAvg, sensorTemperatureAvg_C + offsetTemperature_C, '*-b', 'linewidth', 2, ...
     'linewidth', lineWidth)
-plot(mts.obsTime(indMeso), mts.temperature1p5m_C(indMeso), '*-k', 'linewidth', 2, ...
+plot(mts.obsTime(indMeso), mts.temperature9m_C(indMeso), '*-k', 'linewidth', 2, ...
     'linewidth', lineWidth)
-plot(sensorObsTimeAvg, sensorTemperatureAvg_C + offsetTemperature_C, '*-g', 'linewidth', 2, ...
-    'linewidth', lineWidth)
-plot(timeLim(1)*[1 1], get(gca, 'ylim'), '-k', 'linewidth', lineWidth)
-plot(timeLim(2)*[1 1], get(gca, 'ylim'), '-k', 'linewidth', lineWidth)
 hold off
 set(gca, 'fontsize', fontSize)
 set(gca, 'linewidth', axisWidth)
@@ -321,17 +249,13 @@ end
 figure(3)
 % --------------------------------------
 clf
-plot(sensor.obsTime(indSensor), sensor.humidity_perCent(indSensor), 'r', ...
+plot(sensor.obsTime(indSensor), sensor.humidity_perCent(indSensor) + offsetHumidity_perCent, 'r', ...
     'linewidth', lineWidth)
 hold on
-plot(sensorObsTimeAvg, sensorHumidityAvg_perCent, '*-b', 'linewidth', 2, ...
+plot(sensorObsTimeAvg, sensorHumidityAvg_perCent + offsetHumidity_perCent, '*-b', 'linewidth', 2, ...
     'linewidth', lineWidth)
 plot(mts.obsTime(indMeso), mts.humidity_perCent(indMeso), '*-k', 'linewidth', 2, ...
     'linewidth', lineWidth)
-plot(sensorObsTimeAvg, sensorHumidityAvg_perCent + offsetHumidity_perCent, '*-g', 'linewidth', 2, ...
-    'linewidth', lineWidth)
-plot(timeLim(1)*[1 1], get(gca, 'ylim'), '-k', 'linewidth', lineWidth)
-plot(timeLim(2)*[1 1], get(gca, 'ylim'), '-k', 'linewidth', lineWidth)
 hold off
 set(gca, 'fontsize', fontSize)
 set(gca, 'linewidth', axisWidth)
